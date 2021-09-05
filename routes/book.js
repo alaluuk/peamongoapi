22c5905431f314f60e6ab05b572b1bb6b59faafc
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2021-09-05 13:45:46 +0300

    works in simple mode

---
+++ routes/book.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const book = require('../models/book_model');
+
+router.get('/:name', (req, res) => {
+  let MongoClient = require('mongodb').MongoClient;
+  const url='mongodb://localhost:27017';
+  MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("netdb");
+      dbo.collection("book").findOne({
+          name: req.params.name
+      }, 
+      function(err, result) {
+          if (err) throw err;
+          res.json(result);
+          db.close();
+      });
+  });
+});
+
+/* router.get('/:id?',
+ function(request, response) {
+  if (request.params.id) {
+    book.getById(request.params.id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  } else {
+    book.getAll(function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  }
+}); */
+
+
+router.post('/', 
+function(request, response) {
+  book.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(request.body);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+  book.delete(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.put('/:id', 
+function(request, response) {
+  book.update(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
